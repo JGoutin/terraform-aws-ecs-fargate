93faee70525d1dea8eb02c42866f00c845937699
jgoutin <contact@jgoutin.dev>
2026-01-25 15:11:41 +0100

    fix: Execute command encryption conflict

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -16,7 +16,6 @@ resource "aws_ecs_cluster" "main" {
         kms_key_id = module.kms_key.id
         logging    = "OVERRIDE"
         log_configuration {
-          cloud_watch_encryption_enabled = true
           cloud_watch_log_group_name     = aws_cloudwatch_log_group.execute_command[0].name
         }
       }
